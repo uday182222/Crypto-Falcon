2682bb2da385bc3bb45a8e3b95d3d96f91bbb9a3
Uday Tomar <udaytomar@192.168.1.6>
2025-07-19 01:13:58 +0530

    Fix down_revision reference to deleted migration

--- crypto-backend/alembic/versions/4b21ee4b95d9_create_users_table.py
+++ crypto-backend/alembic/versions/4b21ee4b95d9_create_users_table.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '4b21ee4b95d9'
-down_revision: Union[str, Sequence[str], None] = '080547417010'
+down_revision: Union[str, Sequence[str], None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 

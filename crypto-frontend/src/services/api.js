24335321ae582ef7cb32308cb6e1e7fb217a60a4
Uday Tomar <udaytomar@192.168.1.6>
2025-07-19 01:30:53 +0530

    Update API URL to point to deployed backend

--- crypto-frontend/src/services/api.js
+++ crypto-frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:8000';
+const API_URL = 'https://crypto-falcon-backend.onrender.com';
 
 // Create axios instance with base URL
 const api = axios.create({
